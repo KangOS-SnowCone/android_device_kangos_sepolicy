e2da9763e69e83c7765c64f4e822d5f68962b710
mikeNG <mikeNG@users.noreply.github.com>
2021-10-15 13:16:23 +0530

    sepolicy: CleanUP And Adapt To Android S Build System
    - Adapt to KangOS
    - Refernced from https://github.com/LineageOS/android_device_lineage_sepolicy at lineage-19.0 branch
    
    Change-Id: I674570b3e912fb5073debfa2ea6dd6cf13cd5c04
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Arrow
+# inherit from KangOS
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -9,27 +9,21 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-ifeq ($(TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR),true)
-BOARD_SEPOLICY_M4DEFS += board_excludes_fuseblk_sepolicy=true
-endif
-endif
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/kangos/sepolicy/common/public
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/arrow/sepolicy/common/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/arrow/sepolicy/common/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/kangos/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/system
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/kangos/sepolicy/common/dynamic \
+    device/kangos/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common/dynamic \
-    device/arrow/sepolicy/common/vendor
+    device/kangos/sepolicy/common/dynamic \
+    device/kangos/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/arrow/sepolicy/legacy-common/sepolicy.mk
+-include device/kangos/sepolicy/legacy-common/sepolicy.mk
--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -4,6 +4,6 @@
 #
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/arrow/sepolicy/legacy-common/public/legacy-camera-hal1
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/kangos/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
